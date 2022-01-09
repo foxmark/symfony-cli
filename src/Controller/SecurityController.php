44c74697b37ef1783c9d9433512eed1f2679515a
Pawel Pasich <pawel.pasich@jgbm.co.uk>
2022-01-09 17:04:21 +0000

    adding security [composer require security]

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SecurityController extends AbstractController
+{
+    /**
+     * @Route("/login", name="app_login")
+     */
+    public function login(): Response
+    {
+        //$error = $authenticationUtils->getLastAuthenticationError();
+        //$lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/index.html.twig', []);
+    }
+}
