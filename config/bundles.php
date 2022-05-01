6c7d41feb681da316b9780b7eba04045f15d3263
Pawel Pasich <pawel.pasich@jgbm.co.uk>
2022-05-01 15:31:29 +0100

    DB test in memory

--- config/bundles.php
+++ config/bundles.php
@@ -12,4 +12,5 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
+    DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
 ];
--- tests/Unit/Services/ProductServiceTest.php
+++ tests/Unit/Services/ProductServiceTest.php
@@ -2,10 +2,47 @@
 
 namespace App\Tests\Unit\Services;
 
-use PHPUnit\Framework\TestCase;
+use App\Entity\Product;
+use App\Repository\ProductRepository;
+use App\Tests\DatabasePrimer;
+use Doctrine\ORM\EntityManager;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
-class ProductServiceTest extends TestCase
+class ProductServiceTest extends KernelTestCase
 {
+    protected $entityManager;
+
+    public function setUp(): void
+    {
+        $kernel = self::bootKernel();
+        DatabasePrimer::prime($kernel);
+        /** @var EntityManager entityManager */
+        $this->entityManager = $kernel->getContainer()->get('doctrine')->getManager();
+    }
+
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+
+        $this->entityManager->close();
+        $this->entityManager = null;
+    }
+
+    public function testCanCreateProduct()
+    {
+        $product = new Product();
+        $product->setName('New Product Name')
+            ->setSKU('SKU_' . time())
+            ->setActive(1);
+        $this->entityManager->persist($product);
+        $this->entityManager->flush();
+
+        /** @var EntityManager entityManager */
+        $db = $this->entityManager->getRepository(Product::class);
+        $saved_product = $db->find($product->getId());
+        $this->assertSame($saved_product, $product);
+    }
+
     public function testOne()
     {
         $this->assertTrue(true);
