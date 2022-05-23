c6f309e0517d3e8c17969a335b5bf40d9248596c
Foxmark <pawelpasich@yahoo.co.uk>
2022-05-23 22:11:04 +0100

    composer update + monolog config

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Security\UserRoles;
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,9 +12,10 @@ class AdminController extends AbstractController
     /**
      * @Route("/admin", name="admin_index")
      */
-    public function index()
+    public function index(LoggerInterface $specialLogger)
     {
         $this->denyAccessUnlessGranted(UserRoles::ADMIN);
+        $specialLogger->debug('admin visit');
         return $this->render('admin.html.twig');
     }
 }
